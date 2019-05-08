cebc640c9225ff0aafcf722cfa9cdb38607892df
Huy Dang Tuan <huy.dang@orientsoftware.com>
2019-05-08 16:09:57 +0700

    Master

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    google: {
+        clientID: '572339749933-5i5pmkmig090s9sma5lukjq4f1ecs00p.apps.googleusercontent.com',
+        clientSecret: '2s5ur4e4Zx6KPZdwDXHcsUiw'
+    },
+    mongodb: {
+        dbURI: 'mongodb+srv://huy:<huy>@huydangtuan-xa3ly.mongodb.net/test?retryWrites=true'
+    }
+};
